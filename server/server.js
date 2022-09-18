ac533bc6e1ff67b03f9a51e8311f6e78f40d0071
Marco Glz <marcogonzalezguzman77@gmail.com>
2022-09-18 01:59:38 -0500

    __dirname path.resolve() ERR_DLOPEN_FAILED error

--- server/server.js
+++ server/server.js
@@ -26,13 +26,13 @@ app.use(express.json());
 
 
 // --------------------------deployment------------------------------
-const __dirname = path.resolve();
+//const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(path.resolve(), "/client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(path.resolve(), "client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
