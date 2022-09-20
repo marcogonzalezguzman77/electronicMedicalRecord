8397dd9e19bb7f1ce2b13dd45ea3929dd09a4822
Marco Glz <marcogonzalezguzman77@gmail.com>
2022-09-20 07:50:22 -0500

    NODE_ENV=production correction ERR_DLOPEN_FAILED error

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const path = require('path');
+//const path = require('path');
 //importar variables de entorno locales
-require('dotenv').config({path: '../variables.env'});
-console.log('DB_URL: ',process.env.DB_URL);
+//require('dotenv').config({path: '../variables.env'});
+//console.log('DB_URL: ',process.env.DB_URL);
 
 mongoose.connect(
-  process.env.MONGODB_URI || process.env.DB_URL,
+  process.env.MONGODB_URI || 'DB_URL=mongodb://127.0.0.1:27017/electronic-medical-record',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
